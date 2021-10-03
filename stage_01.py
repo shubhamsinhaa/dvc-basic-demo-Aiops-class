51e01abe8dfafe1019e155442bb4aeeca26232e5
shubham <shubhamkumar283082@gmail.com>
2021-10-03 13:45:29 +0530

    first commit

---
+++ stage_01.py
@@ -0,0 +1,6 @@
+text="input 01"
+
+with open("artifects_01.txt","w+") as f:
+    f.write(text)
+
+ 
---
+++ stage_02.py
@@ -0,0 +1,9 @@
+text="input o2"
+
+
+with open("artifects_01.txt","r") as f:
+    text=f.read()
+
+print(text)
+print("end ofstage 2")
+
